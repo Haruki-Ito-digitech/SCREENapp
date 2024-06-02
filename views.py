cc257cba7e370f7e50770d8b4e410becc4a9d46b
harukin110 <h002002b@mails.cc.ehime-u.ac.jp>
2024-06-02 21:01:20 +0900

    キャプチャ画面共有完了、遅延あり

--- views.py
+++ views.py
@@ -7,13 +7,19 @@ from __init__ import app, socketio
 @app.route('/')
 def index():
     # return send_from_directory('static', 'index.html') # staticフォルダのindex.html
-    return render_template('index.html') # templateフォルダのindex.html
+    return render_template('test.html') # templateフォルダのindex.html
 
 @app.route('/<path:filename>')
 def serve_static(filename):
     return send_from_directory('static', filename)
 
-@socketio.on('message', namespace='/demo')
-def handle_message(message):
-    emit('message', f'{request.sid} => {message}', namespace='/demo', broadcast=True)
+# @socketio.on('message', namespace='/demo')
+# def handle_message(message):
+#     emit('message', f'{request.sid} => {message}', namespace='/demo', broadcast=True)
+
+@socketio.on('video_frame')
+def handle_video_frame(data):
+    frame_data = data['data']
+    print("Received frame data of length:", len(frame_data))
+    emit('video_frame', {'data': frame_data}, broadcast=True)
 
